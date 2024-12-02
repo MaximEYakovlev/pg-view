ab193addadbee9905afd3b19e3a1d5f22ae4853a
Maxim Yakovlev <it.trainee@yandex.ru>
2024-12-02 23:39:35 +0300

    added recursive view

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 'CREATE VIEW unforgettableName AS SELECT someColumn FROM anyTable;'
 
 'CREATE MATERIALIZED VIEW hardToForgetName AS SELECT someColumn FROM anyTable;'
+
+'CREATE RECURSIVE VIEW public.nums_1_100 (n) AS VALUES (1) UNION ALL SELECT n+1 FROM nums_1_100 WHERE n < 100;'
